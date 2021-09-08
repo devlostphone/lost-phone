0573b89f0e38bffa1bc1ca7b956d6852e5a2f8b0
Kenneth Riba <kriba@itteria.cat>
2021-09-08 19:27:41 +0200

    Added QR code generation

--- src/scenes/FakeOS.ts
+++ src/scenes/FakeOS.ts
@@ -59,6 +59,9 @@ export class FakeOS extends FakeOSScene {
         if (password !== null) {
             this.loadState(password[1]);
             this.saveState();
+            this.clearURL();
+        } else {
+            this.loadState();
         }
 
         this.handlerScene.sceneRunning = 'fakeOS';

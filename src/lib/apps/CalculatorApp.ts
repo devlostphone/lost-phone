d5b29352cf093465fc18292d680883e2046c59a0
Xavier Belanche <xbelanch@protonmail.com>
2021-10-11 18:44:44 +0200

    WIP Calculator App (e04)

--- src/lib/apps/CalculatorApp.ts
+++ src/lib/apps/CalculatorApp.ts
@@ -53,6 +53,7 @@ export default class CalculatorApp extends App {
      */
     public render(): void {
         this.showNumericPad();
+        this.showDisplay();
     }
 
     /**
@@ -67,59 +68,89 @@ export default class CalculatorApp extends App {
                 this.danks[i].updateLocation()
         }
     }
-    protected showNumericPad(): void {
 
-        //  Our container
-        var keys:string[] = ['0', '1', '2', '4', '5', '6', '7', '8', '9'];
-        var operators1:string[] = ['÷', 'x', '-', '+'];
-        var operators2:string[] = ['AC', '=', '.', '%', '+/-'];
+    protected showDisplay(): void {
+        // Display
+        this.Display = this.fakeOS.add.text(0, 0, '', { fontFamily: 'Arial', fontSize: '140px', color: '#ffffff' });
+        this.Display.x = this.fakeOS.width - 148;
+        this.Display.y = 128;
+        this.Display.text = '0';
+        this.elements.add(this.Display)
+    }
+
+    protected showNumericPad(): void {
+        var container = this.fakeOS.add.container(0, 0);
         var decimalAdded:boolean = false;
         var sizeButton:number = 64
-        var container = this.fakeOS.add.container(0, 0);
         var colorNumberBackground:number = 0x313131;
         var colorOperatorBackground:number = 0xf69826;
+        var colorTopOperatorBackground:number = 0xa0a0a0;
+
+        // AC, +/-, %, /
+        var buttonAC = this.fakeOS.add.buttonContainer('arc', 'AC', 0, 0, sizeButton, colorTopOperatorBackground);
+        buttonAC.text.setColor("0x000000");
+        buttonAC.text.setFontSize(54);
+        buttonAC.button.onClick = this.buttonPressed.bind(this, buttonAC, 'operator');
+        var buttonSign = this.fakeOS.add.buttonContainer('arc', '+/-', sizeButton * 2.5, 0, sizeButton, colorTopOperatorBackground);
+        buttonSign.text.setColor("0x000000");
+        buttonSign.text.setFontSize(54);
+        buttonSign.button.onClick = this.buttonPressed.bind(this, buttonSign, 'operator');
+        var buttonPercent = this.fakeOS.add.buttonContainer('arc', '%', sizeButton * 2.5 * 2, 0, sizeButton, colorTopOperatorBackground);
+        buttonPercent.text.setColor("0x000000");
+        buttonPercent.text.setFontSize(54);
+        buttonPercent.button.onClick = this.buttonPressed.bind(this, buttonPercent, 'operator');
+        var buttonDivide = this.fakeOS.add.buttonContainer('arc', '÷', sizeButton * 2.5 * 3, 0, sizeButton, colorOperatorBackground);
+        buttonDivide.button.onClick = this.buttonPressed.bind(this, buttonDivide, 'operator');
 
         // 7, 8, 9, x
-        var button7 = this.fakeOS.add.buttonContainer('arc', '7', 0, 0, sizeButton, colorNumberBackground);
+        var button7 = this.fakeOS.add.buttonContainer('arc', '7', 0, sizeButton * 2.5, sizeButton, colorNumberBackground);
         button7.button.onClick = this.buttonPressed.bind(this, button7, 'number');
-        var button8 = this.fakeOS.add.buttonContainer('arc', '8', sizeButton * 2.5, 0, sizeButton, colorNumberBackground);
+        var button8 = this.fakeOS.add.buttonContainer('arc', '8', sizeButton * 2.5, sizeButton * 2.5, sizeButton, colorNumberBackground);
         button8.button.onClick = this.buttonPressed.bind(this, button8, 'number');
-        var button9 = this.fakeOS.add.buttonContainer('arc', '9', sizeButton * 2.5 * 2, 0, sizeButton, colorNumberBackground);
+        var button9 = this.fakeOS.add.buttonContainer('arc', '9', sizeButton * 2.5 * 2, sizeButton * 2.5, sizeButton, colorNumberBackground);
         button9.button.onClick = this.buttonPressed.bind(this, button9, 'number');
-        var buttonMultiply = this.fakeOS.add.buttonContainer('arc', 'x', sizeButton * 2.5 * 3, 0 sizeButton, colorOperatorBackground);
+        var buttonMultiply = this.fakeOS.add.buttonContainer('arc', 'x', sizeButton * 2.5 * 3, sizeButton * 2.5, sizeButton, colorOperatorBackground);
         buttonMultiply.button.onClick = this.buttonPressed.bind(this, buttonMultiply, 'operator');
 
         // 4, 5, 6, -
-        var button4 = this.fakeOS.add.buttonContainer('arc', '4', 0, sizeButton * 2.5, sizeButton, colorNumberBackground);
+        var button4 = this.fakeOS.add.buttonContainer('arc', '4', 0, sizeButton * 2.5 * 2, sizeButton, colorNumberBackground);
         button4.button.onClick = this.buttonPressed.bind(this, button4, 'number');
-        var button5 = this.fakeOS.add.buttonContainer('arc', '5', sizeButton * 2.5, sizeButton * 2.5, sizeButton, colorNumberBackground);
+        var button5 = this.fakeOS.add.buttonContainer('arc', '5', sizeButton * 2.5, sizeButton * 2.5 * 2, sizeButton, colorNumberBackground);
         button5.button.onClick = this.buttonPressed.bind(this, button5, 'number');
-        var button6 = this.fakeOS.add.buttonContainer('arc', '6', sizeButton * 2.5 * 2, sizeButton * 2.5, sizeButton, colorNumberBackground);
+        var button6 = this.fakeOS.add.buttonContainer('arc', '6', sizeButton * 2.5 * 2, sizeButton * 2.5 * 2, sizeButton, colorNumberBackground);
         button6.button.onClick = this.buttonPressed.bind(this, button6, 'number');
-        var buttonMinus = this.fakeOS.add.buttonContainer('arc', '-', sizeButton * 2.5 * 3, sizeButton * 2.5, sizeButton, colorOperatorBackground);
+        var buttonMinus = this.fakeOS.add.buttonContainer('arc', '-', sizeButton * 2.5 * 3, sizeButton * 2.5 * 2, sizeButton, colorOperatorBackground);
         buttonMinus.button.onClick = this.buttonPressed.bind(this, buttonMinus, 'operator');
 
         // 1, 2, 3, +
-        var button3 = this.fakeOS.add.buttonContainer('arc', '3', 0, sizeButton * 2.5 * 2, sizeButton, colorNumberBackground);
-        button3.button.onClick = this.buttonPressed.bind(this, button3, 'number');
-        var button2 = this.fakeOS.add.buttonContainer('arc', '2', sizeButton * 2.5, sizeButton * 2.5 * 2, sizeButton, colorNumberBackground);
-        button2.button.onClick = this.buttonPressed.bind(this, button2, 'number');
-        var button1 = this.fakeOS.add.buttonContainer('arc', '1', sizeButton * 2.5 * 2, sizeButton * 2.5 * 2, sizeButton, colorNumberBackground);
+        var button1 = this.fakeOS.add.buttonContainer('arc', '1', 0, sizeButton * 2.5 * 3, sizeButton, colorNumberBackground);
         button1.button.onClick = this.buttonPressed.bind(this, button1, 'number');
-        var buttonAdd = this.fakeOS.add.buttonContainer('arc', '+', sizeButton * 2.5 * 3, sizeButton * 2.5 * 2, sizeButton, colorOperatorBackground);
+        var button2 = this.fakeOS.add.buttonContainer('arc', '2', sizeButton * 2.5, sizeButton * 2.5 * 3, sizeButton, colorNumberBackground);
+        button2.button.onClick = this.buttonPressed.bind(this, button2, 'number');
+        var button3 = this.fakeOS.add.buttonContainer('arc', '3', sizeButton * 2.5 * 2, sizeButton * 2.5 * 3, sizeButton, colorNumberBackground);
+        button3.button.onClick = this.buttonPressed.bind(this, button3, 'number');
+        var buttonAdd = this.fakeOS.add.buttonContainer('arc', '+', sizeButton * 2.5 * 3, sizeButton * 2.5 * 3, sizeButton, colorOperatorBackground);
         buttonAdd.button.onClick = this.buttonPressed.bind(this, buttonAdd, 'operator');
 
-        // Set container position
-        container.add([ button9, button8, button7, button6, button5, button4, button3, button2, button1, buttonMultiply, buttonMinus, buttonAdd]);
-        this.elements.add(container);
-        container.x = sizeButton * 1.75;
-        container.y = (this.fakeOS.height / 2) - (sizeButton * 2.5);
+        // 0, ., =
+        // TODO: make button 0 as a horizontal capsule shape
+        var button0 = this.fakeOS.add.buttonContainer('arc', '0', 0, sizeButton * 2.5 * 4, sizeButton, colorNumberBackground);
+        button0.button.onClick = this.buttonPressed.bind(this, button0, 'number');
+        var buttonDot = this.fakeOS.add.buttonContainer('arc', '.', sizeButton * 2.5 * 2, sizeButton * 2.5 * 4, sizeButton, colorNumberBackground);
+        buttonDot.button.onClick = this.buttonPressed.bind(this, buttonDot, 'operator');
+        var buttonEqual = this.fakeOS.add.buttonContainer('arc', '=', sizeButton * 2.5 * 3, sizeButton * 2.5 * 4, sizeButton, colorOperatorBackground);
+        buttonEqual.button.onClick = this.buttonPressed.bind(this, buttonEqual, 'operator');
 
-        // Display
-        this.Display = this.fakeOS.add.text(0, 0, '', { fontFamily: 'Arial', fontSize: '92px', color: '#ffffff' });
-        this.Display.x = 32;
-        this.Display.y = 128;
-        this.elements.add(this.Display)
+        // Set container position
+        container.add([ buttonPercent, buttonSign, buttonAC, buttonDivide,
+                        button7, button8, button9, buttonMultiply,
+                        button4, button5, button6, buttonMinus,
+                        button1, button2, button3, buttonAdd,
+                        button0, buttonDot, buttonEqual
+        ]);
+        this.elements.add(container);
+        container.x = sizeButton * 1.9;
+        container.y = (this.fakeOS.height / 2) - (sizeButton * 4);
     }
 
     private numberPressed = (btn: buttonContainer) => {
@@ -128,10 +159,15 @@ export default class CalculatorApp extends App {
 
     private actionPressed = (btn: buttonContainer) => {
         switch(btn.value) {
+            case 'AC':
+            case '%':
+            case '+/-':
             case '+':
             case '-':
             case 'x':
             case '÷':
+            case '.':
+            case '=':
                 this.currentOperator = btn.value;
                 break;
         }

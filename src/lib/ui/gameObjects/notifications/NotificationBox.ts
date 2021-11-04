5c44a73d7a3d479889aed04c372434b1ecd36d6e
Kenneth Riba <kenneth.riba@gmail.com>
2021-11-04 20:17:05 +0100

    Changed notification box

--- src/lib/ui/gameObjects/notifications/NotificationBox.ts
+++ src/lib/ui/gameObjects/notifications/NotificationBox.ts
@@ -28,6 +28,18 @@ export default class NotificationBox extends Phaser.GameObjects.Container
         super(scene, x, y, []);
         this.fakeOS = scene;
         this.id = notification.id;
-        this.add(this.scene.add.text(0, 0, notification.title));
+
+        this.add(this.fakeOS.add.rectangle(
+            0,0,
+            this.fakeOS.getActiveApp().area.width,
+            200,
+            0x666666
+        ));
+
+        this.add(this.fakeOS.add.image(
+            0,0,'lorem-appsum'
+        ).setX(-150));
+
+        this.add(this.fakeOS.add.text(0, 0, notification.title));
     }
  }
